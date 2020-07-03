aa8342473b1888eeec6622e2f58ac7a9a82b86fa
ivanklak <ivanklak17@gmail.com>
2020-07-03 18:58:22 +0300

    class UsersContainer
    Add new class in UsersContainer.jsx.
    Made User a Simple Presentation Component.

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import { connect } from "react-redux";
-import UsersAPIComponent from "./Users";
 import { followAC, unfollowAC, setUserswAC, setCurrentPageAC, setUsersTotalCountAC } from "../../Redux/users-reducer";
+import Users from "./Users";
+import * as Axios from "axios";
 
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+      Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      .then(response => {  
+          this.props.setUsers(response.data.items); 
+          this.props.setTotalUsersCount(response.data.totalCount); 
+     }); 
+    }
+    
+    onPageChanged = (pageNumber) => {
+      this.props.setCurrentPage(pageNumber);
+      Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+      .then(response => {  
+          this.props.setUsers(response.data.items); 
+     }); 
+    }
+  
+    render() {
+      return <Users totalUsersCount={this.props.totalUsersCount}
+      pageSize={this.props.pageSize}
+      currentPage={this.props.currentPage}
+      onPageChanged={this.onPageChanged}
+      users={this.props.users}
+      follow={this.props.follow}
+      unfollow={this.props.unfollow}
+      /> 
+    }
+  }
+  
 let mapStateToProps = state => {
   return {
     users: state.usersPage.users,
@@ -32,4 +63,4 @@ let mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
