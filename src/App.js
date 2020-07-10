2a1b6b9f2015390e4a5d3bbf196f506c1d83cf95
ivanklak <ivanklak17@gmail.com>
2020-07-10 13:16:56 +0300

    withRouter
    ajax: profile+userId

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = props => {
         <Route path="/dialogs" 
         render={() => <DialogsContainer />} />
 
-        <Route path="/profile" 
+        <Route path="/profile/:userId?" 
         render={() => <ProfileContainer />} />
 
         <Route path="/users" 
