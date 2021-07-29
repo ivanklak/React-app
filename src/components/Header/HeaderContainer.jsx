ded139a6f22e616fd8586b0c5ef330a4be77e4e2
ivanklak <ivanklak17@gmail.com>
2021-07-29 14:21:03 +0300

    refactor: rebuilt HeaderContainer into functional component

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import Header from "./Header";
-import Axios from "axios";
 import { connect } from "react-redux";
-import { getAuthUserData, logout } from "../../Redux/auth-reducer";
-import { authAPI} from "../../api/api";
+import { logout } from "../../Redux/auth-reducer";
 
-class HeaderContainer extends React.Component {
-  // componentDidMount() {
-  //   //authAPI.me();
-  //   this.props.getAuthUserData();
-  // }
-
-  render() {
-    return <Header {...this.props} />;
-  }
-}
+const HeaderContainer = props => {
+  return <Header {...props} />;
+};
 
 const mapStateToProps = state => ({
   isAuth: state.auth.isAuth,
-  login: state.auth.login,
+  login: state.auth.login
 });
 
 export default connect(mapStateToProps, { logout })(HeaderContainer);
