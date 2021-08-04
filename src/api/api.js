2d63b401dfd8e3d5c0a9638988e007d413a25248
ivanklak <ivanklak17@gmail.com>
2021-08-04 15:28:54 +0300

    refactor: add blank lines after blocks

--- src/api/api.js
+++ src/api/api.js
@@ -12,12 +12,15 @@ export const usersAPI = {
   getUsers(currentPage = 1, pageSize = 100) {
     return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data);
   },
+
   toFollow(userId) {
     return instance.post(`follow/${userId}`).then(response => response.data);
   },
+
   toUnfollow(userId) {
     return instance.delete(`follow/${userId}`).then(response => response.data);
   },
+
   getProfile(userId) {
     console.warn(`Obsolete method. Please use profileAPI object.`);
 
@@ -29,9 +32,11 @@ export const profileAPI = {
   getProfile(userId) {
     return instance.get(`profile/` + userId);
   },
+
   getStatus(userId) {
     return instance.get(`profile/status/` + userId);
   },
+
   updateStatus(status) {
     return instance.put(`profile/status`, {status: status});
   },
@@ -41,9 +46,11 @@ export const authAPI = {
   me() {
     return instance.get(`auth/me`);
   },
+
   login(email, password, rememberMe = false) {
     return instance.post(`auth/login`, {email, password, rememberMe});
   },
+
   logout() {
     return instance.delete(`auth/login`);
   },
