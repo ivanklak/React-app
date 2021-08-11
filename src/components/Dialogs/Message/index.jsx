4b1700365aa2fe9228c2e214731720d7af17026f
ivanklak <ivanklak17@gmail.com>
2021-08-11 10:29:06 +0300

    refactor: remove extra div

--- src/components/Dialogs/Message/index.jsx
+++ src/components/Dialogs/Message/index.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 
 import styles from '../styles.module.css';
 
-const Message = props => (
-  <div>
-    <div className={styles.dialog}>{props.message}</div>
-  </div>
-);
+const Message = props => <div className={styles.dialog}>{props.message}</div>;
 
 export default Message;
