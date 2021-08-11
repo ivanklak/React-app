aa5d7ae05e54171792852d199785cd35166365a1
ivanklak <ivanklak17@gmail.com>
2021-08-11 09:46:33 +0300

    refactor: move MessageForm to a separate component

---
+++ src/components/Dialogs/MessageForm.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Field, reduxForm} from 'redux-form';
+
+import {Textarea} from '../common/FormsControl';
+import {maxLengthCreator, required} from '../../utils/validators';
+
+import styles from './styles.module.css';
+
+const maxLength50 = maxLengthCreator(50);
+
+const AddMessageForm = props => (
+  <form onSubmit={props.handleSubmit}>
+    <div className={styles.form}>
+      <Field component={Textarea} validate={[required, maxLength50]} name="newMessageBody" placeholder="Enter your message" className={styles.messageInput} />
+    </div>
+    <div>
+      <button>Send</button>
+    </div>
+  </form>
+);
+
+const AddMessageFormRedux = reduxForm({form: 'dialogAddMessageForm'})(AddMessageForm);
+
+export default AddMessageFormRedux;
--- src/components/Dialogs/index.jsx
+++ src/components/Dialogs/index.jsx
@@ -1,34 +1,16 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
-import {Field, reduxForm} from 'redux-form';
 import {useDispatch, useSelector} from 'react-redux';
 
-import {Textarea} from '../common/FormsControl';
-import {required, maxLengthCreator} from '../../utils/validators';
 import {sendMessages} from '../../Redux/dialogs-reducer';
 import {getAuth} from '../../selectors';
 
 import DialogItem from './DialogItem';
 import Message from './Message';
 import selector from './selector';
+import AddMessageFormRedux from './MessageForm';
 
 import styles from './styles.module.css';
-
-//todo move to another component
-const maxLength50 = maxLengthCreator(50);
-
-const AddMessageForm = props => (
-  <form onSubmit={props.handleSubmit}>
-    <div className={styles.form}>
-      <Field component={Textarea} validate={[required, maxLength50]} name="newMessageBody" placeholder="Enter your message" className={styles.messageInput} />
-    </div>
-    <div>
-      <button>Send</button>
-    </div>
-  </form>
-);
-
-const AddMessageFormRedux = reduxForm({form: 'dialogAddMessageForm'})(AddMessageForm);
 
 const Dialogs = () => {
   const isAuth = useSelector(getAuth);
