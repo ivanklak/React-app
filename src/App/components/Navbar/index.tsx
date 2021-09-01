0bba07b44402ff572d0c742ef083db1d56e0989b
ivanklak <ivanklak17@gmail.com>
2021-09-01 09:37:53 +0300

    refactor: update Navbar

--- src/App/components/Navbar/index.tsx
+++ src/App/components/Navbar/index.tsx
@@ -1,25 +1,10 @@
-import React, {FC, useEffect, useState} from 'react';
-import {Link, useLocation, useHistory} from 'react-router-dom';
+import React, {FC} from 'react';
+import {Link, useLocation} from 'react-router-dom';
 import {Layout, Menu} from 'antd';
 
 import styles from './styles.module.css';
 
 const {Sider} = Layout;
-
-interface IClickEvent {
-  key: string;
-}
-
-const pages = [
-  {key: '/profile', path: '/profile'},
-  {key: '/dialogs', path: '/dialogs'},
-  {key: '/users', path: '/users'},
-  {key: '/friends', path: '/friends'},
-  {key: '/news', path: '/news'},
-  {key: '/music', path: '/music'},
-  {key: '/settings', path: '/settings'},
-  {key: '/login', path: '/login'},
-];
 
 const menuItems = [
   {key: '/profile', label: 'Profile', path: '/profile'},
@@ -33,28 +18,10 @@ const menuItems = [
 
 const Navbar: FC = () => {
   const location = useLocation();
-  const history = useHistory();
-  const [selectedPage, setSelectedPage] = useState('/profile');
-
-  const handleClick = ({key}: IClickEvent) => {
-    const clicked = menuItems.find(item => item.key === key);
-
-    return clicked ? history.push(clicked.path) : history.push('/profile');
-  };
-
-  useEffect(() => {
-    const startPage = pages.find(item => location.pathname.startsWith(item.path))?.key;
-
-    if (startPage !== undefined) {
-      setSelectedPage(startPage);
-    } else {
-      history.push('/profile');
-    }
-  }, [location]);
 
   return (
     <Sider className={styles.siderContainer}>
-      <Menu mode="inline" selectedKeys={[selectedPage]} onClick={handleClick} className={styles.menuContainer}>
+      <Menu mode="inline" defaultSelectedKeys={[location.pathname]} className={styles.menuContainer}>
         {menuItems.map(item => (
           <Menu.Item key={item.key}>
             <Link to={item.path}>{item.label}</Link>
