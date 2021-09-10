9847bdf1eff13a341b641c9a7288e96fe6acb15b
ivanklak <ivanklak17@gmail.com>
2021-09-10 16:12:10 +0300

    refactor: add PostForm test

---
+++ src/Profile/components/MyPosts/PostForm/index.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import '../../../../matchMedia';
+import store from '../../../../App/redux-store';
+
+import PostForm from './index';
+
+const createTestables = (props: any) => {
+  const renderResult = render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <PostForm {...props} />
+      </Provider>
+    </BrowserRouter>,
+  );
+
+  return renderResult;
+};
+
+describe('PostForm Component', () => {
+  it('show the sent post', async () => {
+    const onAddPost = jest.fn();
+    const textareaValue = 'This is my third post';
+
+    const {getByTestId} = createTestables({onAddPost});
+
+    const textarea = getByTestId('NewPost.Input');
+    const submitButton = getByTestId('NewPost.Submit');
+
+    fireEvent.change(textarea, {target: {value: textareaValue}});
+    fireEvent.click(submitButton);
+
+    setTimeout(() => {
+      expect(onAddPost).toHaveBeenCalledTimes(1);
+    }, 0);
+  });
+});
