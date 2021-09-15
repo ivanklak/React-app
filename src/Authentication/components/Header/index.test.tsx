f2fb02119e033cd7399364768dbcd9b59d980fb6
ivanklak <ivanklak17@gmail.com>
2021-09-15 13:51:28 +0300

    refactor: add Header tests

---
+++ src/Authentication/components/Header/index.test.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import '../../../matchMedia';
+//@ts-ignore
+import {fireEvent, render, wait} from '@testing-library/react';
+
+import store from '../../../App/redux-store';
+
+import {AuthenticationActions} from '../../actions';
+import {IAuthenticationsData} from '../../types';
+import {authAPI} from '../../services';
+
+import HeaderApp from './index';
+
+const authData: IAuthenticationsData = {
+  userId: 9208,
+  email: 'ivanklak17@gmail.com',
+  login: 'ivanklak',
+  isAuth: true,
+};
+const logoutData: IAuthenticationsData = {
+  userId: null,
+  email: null,
+  login: null,
+  isAuth: false,
+};
+
+const createTestables = (props: Partial<any>) => {
+  const renderResult = render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <HeaderApp {...props} />
+      </Provider>
+    </BrowserRouter>,
+  );
+
+  return {...renderResult, store};
+};
+
+describe('Header Component', () => {
+  let mockedLogout: jest.SpyInstance;
+  const dispatch = store.dispatch;
+
+  beforeEach(() => {
+    mockedLogout = jest.spyOn(authAPI, 'logout');
+    store.dispatch = jest.fn(dispatch);
+  });
+
+  it('should be rendered', () => {
+    const {getByTestId} = createTestables({});
+
+    const header = getByTestId('Header.Title');
+
+    expect(header).toBeInTheDocument();
+  });
+
+  it('Login button should be displayed when not logged', () => {
+    const {getByTestId} = createTestables({});
+
+    const loginButton = getByTestId('LoginUser.Submit');
+
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  it('users avatar and Logout button should be displayed when logged', () => {
+    store.dispatch(AuthenticationActions.getAuthUserDataSuccess(authData));
+
+    const {getByTestId} = createTestables({});
+
+    const userAvatar = getByTestId('LoginUser.Img');
+    const logoutButton = getByTestId('LogoutUser.Submit');
+
+    expect(userAvatar).toBeInTheDocument();
+    expect(logoutButton).toBeInTheDocument();
+  });
+
+  it('click on logout button', async () => {
+    store.dispatch(AuthenticationActions.getAuthUserDataSuccess(authData));
+
+    const {getByTestId} = createTestables({});
+
+    const logoutButton = getByTestId('LogoutUser.Submit');
+
+    fireEvent.click(logoutButton);
+
+    await wait(() => expect(mockedLogout).toHaveBeenCalledTimes(1));
+
+    store.dispatch(AuthenticationActions.getAuthUserDataSuccess(logoutData));
+
+    const loginButton = getByTestId('LoginUser.Submit');
+
+    expect(loginButton).toBeInTheDocument();
+  });
+});
