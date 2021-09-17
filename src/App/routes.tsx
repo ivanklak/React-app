daab33ba2bee9481890f5d8c22d84a632ab57332
ivanklak <ivanklak17@gmail.com>
2021-09-17 11:56:49 +0300

    refactor: fix routes.tsx

--- src/App/routes.tsx
+++ src/App/routes.tsx
@@ -13,18 +13,16 @@ import Settings from './components/Settings';
 import Friends from './components/Friends';
 
 const Routes = () => (
-  <>
-    <Switch>
-      <Route path="/dialogs" component={withAuthRedirect(Dialogs)} />
-      <Route path="/profile/:userId?" component={Profile} />
-      <Route path="/users" component={withAuthRedirect(Users)} />
-      <Route path="/login" component={Login} />
-      <Route path="/news" component={News} />
-      <Route path="/music" component={Music} />
-      <Route path="/settings" component={Settings} />
-      <Route path="/friends" component={Friends} />
-    </Switch>
-  </>
+  <Switch>
+    <Route path="/dialogs" component={withAuthRedirect(Dialogs)} />
+    <Route path="/profile/:userId?" component={Profile} />
+    <Route path="/users" component={withAuthRedirect(Users)} />
+    <Route path="/login" component={Login} />
+    <Route path="/news" component={News} />
+    <Route path="/music" component={Music} />
+    <Route path="/settings" component={Settings} />
+    <Route path="/friends" component={Friends} />
+  </Switch>
 );
 
 export default Routes;
