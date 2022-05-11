d8091fd534a3378752027699acb59d0c26a739e2
ivanklak <ivanklak17@gmail.com>
2022-05-11 17:07:45 +0300

    refactor: add new changes

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import store from './App/redux-store';
 import App from './App';
 
 //new changes in new branch
+//second part of changes in the new branch after `git pull` in `master`
 
 ReactDOM.render(
   <BrowserRouter>
